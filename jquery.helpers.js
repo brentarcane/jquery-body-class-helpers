b5a57e9e675ec8e1fc4455a3b482fddd22a101f3
Brent Kozjak <brent.kozjak@covata.com>
2016-08-12 15:56:06 +0100

    initial commit

---
+++ jquery.helpers.js
@@ -0,0 +1,37 @@
+(function($) {
+
+    'use strict';
+
+    var Helper = function() {
+        this.$body = $('body');
+        this.setUserOS();
+        this.setUserAgent();
+    }
+
+    // Set environment vars
+    Helper.prototype.setUserOS = function() {
+        var OSName = "";
+        if (navigator.appVersion.indexOf("Win") != -1) OSName = "windows";
+        if (navigator.appVersion.indexOf("Mac") != -1) OSName = "mac";
+        if (navigator.appVersion.indexOf("X11") != -1) OSName = "unix";
+        if (navigator.appVersion.indexOf("Linux") != -1) OSName = "linux";
+
+        this.$body.addClass(OSName);
+    }
+
+    Helper.prototype.setUserAgent = function() {
+        if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)) {
+            this.$body.addClass('mobile');
+        } else {
+            this.$body.addClass('desktop');
+            if (navigator.userAgent.match(/MSIE 9.0/)) {
+                this.$body.addClass('ie9');
+            }
+        }
+    }
+
+    $.Helper = new Helper();
+    $.Helper.Constructor = Helper;
+
+})(window.jQuery);
+
